d531fb983e82d601794492425dd5e88010cc5de9
Danilo Silva <danilo.324bs@gmail.com>
2024-09-01 16:48:15 -0500

    Initial commit

---
+++ archivo1.py
@@ -0,0 +1,5 @@
+age = int(input('Enter your age in numbers: '))
+if age >= 18:
+    print('You are an adult')
+else:
+    print('You are a minor')
---
+++ archivo2.py
@@ -0,0 +1,6 @@
+password = 'HelloWorld24'
+user_entrance = input('Enter the password ')
+if user_entrance.lower() == password.lower():
+    print('Acces success')
+else:
+    print('Acces denied')
